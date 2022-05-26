b626566b83579cfe81a8c9dd265a48693d0039e0
Sergio Rodríguez Sanz <80534752+Srsanz@users.noreply.github.com>
2022-05-26 12:59:10 +0200

    Add files via upload

---
+++ Entrega Final/Arduino/ArduinoAlarma.ino
@@ -0,0 +1,81 @@
+#define trig 2 // emisor señal
+#define echo 3 // receptor señal
+#define buzzer 12 // alarma
+char caracter;
+  String comando;
+  float duracion, distancia;
+
+
+void setup() {
+  pinMode(trig, OUTPUT); // emisor
+  pinMode(echo, INPUT); // receptor
+  pinMode(buzzer, OUTPUT); 
+
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  
+  while (Serial.available() > 0)
+  {
+    caracter = Serial.read();
+    comando.concat(caracter);
+    delay(10);
+  }
+  if (comando.equals("a") == true)
+  {
+        digitalWrite(trig, LOW);
+        delay(2);
+  
+        digitalWrite(trig, HIGH);
+        delay(10);
+        digitalWrite(trig, LOW);
+  
+        duracion = pulseIn(echo, HIGH);  // calculo de la distancia
+        distancia = (duracion/2)*0.0343;  // en este caso la medimos en cm/ms
+        Serial.println(distancia);
+
+        if (distancia < 20)  // x será la distancia que medirá el sensor en reposo
+        {
+          tone(buzzer, 1000);  // alarma suena con fecuencia de 1000 Hz
+          delay(200); // la alarma sonará durante 1 segundo
+
+          tone(buzzer, 3000);
+          delay(200);
+
+          tone(buzzer, 1000);  // alarma suena con fecuencia de 1000 Hz
+          delay(200); // la alarma sonará durante 1 segundo
+    
+          tone(buzzer, 3000);
+          delay(200);
+
+          tone(buzzer, 1000);  // alarma suena con fecuencia de 1000 Hz
+          delay(200); // la alarma sonará durante 1 segundo
+
+          tone(buzzer, 3000);
+          delay(200);
+
+          tone(buzzer, 1000);  // alarma suena con fecuencia de 1000 Hz
+          delay(200); // la alarma sonará durante 1 segundo
+    
+          tone(buzzer, 3000);
+          delay(200);
+
+          tone(buzzer, 1000);  // alarma suena con fecuencia de 1000 Hz
+          delay(200); // la alarma sonará durante 1 segundo
+
+          tone(buzzer, 3000);
+          delay(200);
+
+          Serial.write("c");
+
+          } else {
+            noTone(buzzer); // la alarma no suena
+          }
+          
+  
+}
+ 
+}
